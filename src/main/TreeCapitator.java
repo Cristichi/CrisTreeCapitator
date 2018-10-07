c0b13b5b12ebb1100f6f1e87f5c43e0a3a2e25e4
Cristichi <cristichi@hotmail.es>
2018-10-07 12:29:47 +0200

    empezamos el onCommand

--- src/main/TreeCapitator.java
+++ src/main/TreeCapitator.java
@@ -4,11 +4,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import updater.Updater;
+import updater.Updater.ReleaseType;
 
 public class TreeCapitator extends JavaPlugin implements Listener {
 	private PluginDescriptionFile desc = getDescription();
@@ -18,9 +23,29 @@ public class TreeCapitator extends JavaPlugin implements Listener {
 	// Ajustes
 	private int maxBlocks = 1500;
 
+	// Updater
+	private static final int ID = 294976;
+	public static boolean update = false;
+	public static String name = "";
+	public static ReleaseType type = null;
+	public static String version = "";
+	public static String link = "";
+
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+
+		Updater updater = new Updater(this, ID, this.getFile(), Updater.UpdateType.NO_DOWNLOAD, false);
+		update = updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE;
+		name = updater.getLatestName();
+		version = updater.getLatestGameVersion();
+		type = updater.getLatestType();
+		link = updater.getLatestFileLink();
+
+		if (update) {
+			getLogger().info("An update is available, use /tc update to update to the lastest version");
+		}
+
 		getLogger().info("Enabled");
 	}
 
@@ -35,8 +60,8 @@ public class TreeCapitator extends JavaPlugin implements Listener {
 		Material tipo = primero.getBlockData().getMaterial();
 
 		int destr = breakRec(primero, tipo, 0);
-		
-		e.getPlayer().sendMessage(header+"You destroyed "+destr+" blocks of "+tipo.name()+".");
+
+		e.getPlayer().sendMessage(header + "You destroyed " + destr + " blocks of " + tipo.name() + ".");
 	}
 
 	private int breakRec(Block lego, Material type, int destroyed) {
@@ -46,21 +71,56 @@ public class TreeCapitator extends JavaPlugin implements Listener {
 		}
 
 		lego.breakNaturally();
-		if (++destroyed >= maxBlocks && maxBlocks>0) {
+		if (++destroyed >= maxBlocks && maxBlocks > 0) {
 			return destroyed;
 		}
 
 		World mundo = lego.getWorld();
 		int x = lego.getX(), y = lego.getY(), z = lego.getZ();
 
-		destroyed = breakRec(mundo.getBlockAt(x+1, y, z), type, destroyed);
-		destroyed = breakRec(mundo.getBlockAt(x, y+1, z), type, destroyed);
-		destroyed = breakRec(mundo.getBlockAt(x, y, z+1), type, destroyed);
+		destroyed = breakRec(mundo.getBlockAt(x + 1, y, z), type, destroyed);
+		destroyed = breakRec(mundo.getBlockAt(x, y + 1, z), type, destroyed);
+		destroyed = breakRec(mundo.getBlockAt(x, y, z + 1), type, destroyed);
 
-		destroyed = breakRec(mundo.getBlockAt(x-1, y, z), type, destroyed);
-		destroyed = breakRec(mundo.getBlockAt(x, y-1, z), type, destroyed);
-		destroyed = breakRec(mundo.getBlockAt(x, y, z-1), type, destroyed);
-		
+		destroyed = breakRec(mundo.getBlockAt(x - 1, y, z), type, destroyed);
+		destroyed = breakRec(mundo.getBlockAt(x, y - 1, z), type, destroyed);
+		destroyed = breakRec(mundo.getBlockAt(x, y, z - 1), type, destroyed);
+
 		return destroyed;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		boolean bueno = label.equals(command.getLabel());
+		String[] cmds = command.getAliases().toArray(new String[]{});
+		for (int i = 0; i < cmds.length&&!bueno; i++) {
+			if (label.equals(cmds[i])) {
+				bueno = true;
+			}
+		}
+
+		boolean sinPermiso = true;
+		if (bueno) {
+			if (args.length>0) {
+				switch (args[0]) {
+				case "update":
+					if (sender.hasPermission("cristreecapitator.admin")) {
+						sinPermiso= false;
+					}
+					break;
+
+				default:
+					break;
+				}
+			}else {
+				return false;
+			}
+		}
+
+		if (sinPermiso) {
+			sender.sendMessage(header+ChatColor.RED+"You don't have permission to use this.");
+		}
+		return bueno;
 	}
 }
